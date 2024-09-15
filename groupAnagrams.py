1954d9d0a710f6e72c5b166f0442949fd7d02eb3
BeautifulSosalochka <valerie.gassiev@bk.ru>
2024-09-15 23:42:30 +0300

    Added Implementation of Group Anagrams

---
+++ groupAnagrams.py
@@ -0,0 +1,34 @@
+
+print("\n-----LeetCode требует реализацию через классы-----\n")
+
+class Solution:
+    def __init__(self, words):
+        self.words = words
+        self.result = []
+    def __getitem__(self, index):
+        return self.words[index]
+    def group_anagrams(self):
+        anagram_dict = {}
+        for i in range(len(self.words)):
+            word = self[i]
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in anagram_dict:
+                anagram_dict[sorted_word].append(word)
+            else:
+                anagram_dict[sorted_word] = [word]
+        self.result = list(anagram_dict.values())
+
+        print(f"Исходный массив: {self.words}\n")
+        print(f"Отсортированный: {self.result}")
+
+
+string_arr = ["eat","tea","tan","ate","nat","bat"]
+
+
+def main():
+    result = Solution(string_arr)
+    result.group_anagrams()
+
+
+if __name__ == "__main__":
+    main()
